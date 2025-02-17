cabe336c214eb1e447e93265b791a671a1cc0e23
Iman Nurohman <imannurohman1@gmail.com>
2025-02-17 23:04:54 +0700

    Initial commit

---
+++ config-server/src/test/java/com/imannrhman/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imannrhman.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
